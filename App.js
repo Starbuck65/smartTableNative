c1000c8d8c7ef6835a363aff66ca4ed3a17b0344
Starbuck65 <javier.h.poncell@gmail.com>
2018-08-09 16:26:19 +0200

    Basic skeleton. Connection to server and tag recognition.

---
+++ App.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import MaterialHandler from './materialHandler.js';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.body}>
+          <MaterialHandler />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+  body: {
+    flex: 1,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ README.md
@@ -0,0 +1,16 @@
+# smartTableNative
+
+## Installing
+
+TBD
+
+## Project Description
+This is the Android application for handling the smart table project. 
+
+
+## To-Do
+- [ ] UX/UI design
+- [ ] Frontend wireframe
+- [ ] Backend
+- [ ] Testing
+- [ ] Deploy
